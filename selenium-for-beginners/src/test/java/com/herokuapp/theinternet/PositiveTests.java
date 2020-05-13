41fff93da130b80485080420d6e9ee3ccf336512
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-13 18:28:04 -0400

    chromedriver

--- selenium-for-beginners/src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,11 +1,13 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.WebDriver;
+
 public class PositiveTests {
 
 	public void loginTest() {
 		// create driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-		Webdriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		//		open test page
 //		enter userName
 //		enter password
