2f0e2df657d1e9d8aaa6b06095e3d567da3c056f
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-14 17:58:40 -0400

    add string url

--- selenium-for-beginners/src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -10,6 +10,7 @@ public class PositiveTests {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		//		open test page
+		String url = "https://the-internet.herokuapp.com/login";
 		driver.get(url);
 //		enter userName
 //		enter password
