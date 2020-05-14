fa1e82b82590c3a80dab99925c80e7b04d96017d
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-14 19:16:43 -0400

    add driver quit

--- selenium-for-beginners/src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -2,27 +2,34 @@ package com.herokuapp.theinternet;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class PositiveTests {
-
+	
+	@Test
 	public void loginTest() {
+		System.out.println("Start logintest");
+		
+		
 		// create driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		// sleep for 3 seconds
+		sleep(3000);
+				
+		// maximize browser window
+		driver.manage().window().maximize();
+		
 		//		open test page
 		String url = "https://the-internet.herokuapp.com/login";
 		driver.get(url);
+		System.out.println("page is opened");
 		
-		// sleep for 3 seconds
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// sleep for 2 seconds
+		sleep(2000);
 		
-		// maximize browser window
-		driver.manage().window().maximize();
+		
 //		enter userName
 //		enter password
 //		click login button
@@ -31,6 +38,19 @@ public class PositiveTests {
 //			new URL
 //			logout button
 //			successful message
+		// close browser
+		driver.quit();
+		
+		
+	}
+
+	private void sleep(long m) {
+		try {
+			Thread.sleep(m);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
